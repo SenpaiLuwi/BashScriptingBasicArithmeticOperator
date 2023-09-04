28d813b5ae0ecdc024b5d709939968fd189d0478
John Louie Ubias <johnlouieubiasxd50@gmail.com>
2023-09-04 17:11:49 +0800

    Basic Arithmetic Operator

---
+++ ArithmeticOperator.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+name="(You can put your name here)"
+program="(You can put your program here)"
+level="(You can put your year level here)"
+echo $name $program $level
+
+#!/bin/bash
+Varl="Hello"
+Var2="(You can put your name here)"
+echo $Var1 $Var2
+
+#!bin/bash
+read -p 'Please Enter Your Name: namel
+echo Your Name is $namel
+
+#!/bin/bash
+read -p 'Please Enter Your Age:' agel
+read -p 'Please Enter Your Birthday:' birthdayl
+echo Your Age is $agel and Your Birthday is on $birthdayl
+
+
+# ! /bin/bash
+#Addition
+expr $x + $y
+
+#Subtraction
+expr $x -$y
+
+#Multiplication
+expr $x \* $y
+
+#Division
+expr $x / $y
+
+#Exponentiation
+echo $a((x**y))
+echo $b
+
+#Modular Division
+echo $b=$((x%y))
+echo $b
+
+#lncrementing x by 5
+c=$((x+=y))
+echo $c
+
+#Decrementing x by 5
+d=$((x-=y))
+echo Sd
+
+#Multiply x by 5
+e=$((x*5))
+echo $e
+
+#Dividing x by 5
+f=$((x/5))
+echo $f
+
+#Remainder of dividing x by 5
+remainder=$((x % 5))
+echo $remainder
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
